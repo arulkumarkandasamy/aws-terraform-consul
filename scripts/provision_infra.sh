30ca43665c84bfea9fc1501fd35cd3fef625b0f2
Arulkumar Kandasamy <arulkumar4848@yahoo.co.uk>
2017-09-03 20:24:08 +0100

    first commit

---
+++ scripts/provision_infra.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Provision AWS infrastructure
+
+set -e
+
+echo " "
+echo "*** Building AWS infrastructure ***"
+
+cd ../tf_env_aws/
+
+terraform remote config \
+  -backend=s3 \
+  -backend-config="bucket=tf-remote-state-gstafford" \
+  -backend-config="key=terraform_consul.tfstate" \
+  -backend-config="region=us-east-1"
+
+terraform plan -no-color
+terraform apply -no-color
+
+echo " "
+echo "*** Building AWS infrastructure complete ***"
